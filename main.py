ac0d068e2bc24c536aa9fd19d0ff0a09edc6cdd9
ivane98 <i.vardoshvili22@gmail.com>
2024-06-26 13:01:15 +0400

    first

---
+++ main.py
@@ -0,0 +1,61 @@
+#VARIABLES
+
+# first_name = 'ivane'
+# last_name = 'Vardoshvili'
+
+# full_name = first_name + " " + last_name
+
+# print(full_name)
+
+# age = 25
+# age +=1
+
+# print('here is my age:' + str(age))
+
+#same stuff for floats and bools
+
+# name, age, attractive = 'ivane', 25, True
+
+# spongebob = squidward = sandy = patric = 30
+
+# print(spongebob, squidward, sandy, patric)
+
+# STRINGS
+
+# name = 'Bro'
+
+# print(name)
+# print(len(name))
+# print(name.find("I"))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())
+# print(name.isalpha())
+# print(name.count("i"))
+# print(name.replace("o", "a"))
+# print(name*10)
+
+# TYPECASTING
+
+# x = 1
+# y = 2.0
+# z = '3'
+
+# x = str(x)
+# y = str(y)
+# z = str(z)
+
+# print(x)
+# print("hello:" + y)
+# print(z*3)
+
+###User Input
+
+name = input("What is your name: ")
+age = int(input('How old are you: '))
+height = float(input("What is your height: "))
+
+print("Hello " + name)
+print("you are " + str(age) + " years old")
+print("your height is " + str(height) + " cm")
