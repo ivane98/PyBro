74e3468302a4ab208a15d4eac885826f110010ee
ivane98 <i.vardoshvili22@gmail.com>
2024-06-30 18:01:51 +0400

    'finished'

--- projects/text_editor.py
+++ projects/text_editor.py
@@ -5,34 +5,61 @@ from tkinter.messagebox import *
 from tkinter.filedialog import *
 
 def change_color():
-    pass
+    color = colorchooser.askcolor(title='choose color')
+    text_area.config(fg=color[1])
 
 def change_font(*args):
-    pass
+    text_area.config(font=(font_name.get(), size_box.get()))
 
 def new_file():
-    pass
+    window.title('Utitled')
+    text_area.delete(1.0, END)
 
 def open_file():
-    pass
+    file = askopenfilename(defaultextension='.txt', file=[('All Files', '*.*'), ('Text Documents', '*.txt')])
+
+    try:
+        window.title(os.path.basename(file))
+        text_area.delete(1.0, END)
+
+        file = open(file, 'r')
+
+        text_area.insert(1.0, file.read())
+    except Exception:
+        print('cant open a file')
+    finally:
+        file.close()
 
 def save_file():
-    pass
+    file = filedialog.asksaveasfilename(initialfile='untitled.txt', defaultextension='.txt', filetypes=[('All Files', '*.*'), ('Text Documents', '*.txt')])
+
+    if file is None:
+        return
+    else:
+        try:
+            window.title(os.path.basename(file))
+            file = open(file, 'w')
+
+            file.write(text_area.get(1.0, END))
+        except Exception:
+            print('cant save a file')
+        finally:
+            file.close()
 
 def cut():
-    pass
+    text_area.event_generate('<<Cut>>')
 
 def copy():
-    pass
+    text_area.event_generate('<<Copy>>')
 
 def paste():
-    pass
+    text_area.event_generate('<<Paste>>')
 
 def about():
-    pass
+    showinfo('About this program', 'text editor written by you')
 
 def quit():
-    pass
+    window.destroy()
 
 
 window = Tk()
@@ -59,8 +86,45 @@ scroll_bar = Scrollbar(text_area)
 window.grid_rowconfigure(0, weight=1)
 window.grid_columnconfigure(0, weight=1)
 text_area.grid(sticky=N + E + S + W)
+scroll_bar.pack(side=RIGHT, fill=Y)
+text_area.config(yscrollcommand=scroll_bar.set)
+
+frame = Frame()
+frame.grid()
+
+color_button = Button(frame, text='color', command=change_color)
+color_button.grid(row=0, column=0)
+
+font_box = OptionMenu(frame, font_name, *font.families(), command=change_font)
+font_box.grid(row=0, column=1)
+
+size_box = Spinbox(frame, from_=1, to=100, textvariable=font_size, command=change_font)
+size_box.grid(row=0, column=2)
 
 scroll_bar.pack(side=RIGHT, fill=Y)
 text_area.config(yscrollcommand=scroll_bar.set)
+
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label='File', menu=file_menu)
+file_menu.add_command(label='New', command=new_file)
+file_menu.add_command(label='Open', command=open_file)
+file_menu.add_command(label='Save', command=save_file)
+file_menu.add_separator()
+file_menu.add_command(label='Quit', command=quit)
+
+edit_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label='Edit', menu=edit_menu)
+edit_menu.add_command(label='Cut', command=cut)
+edit_menu.add_command(label='Copy', command=copy)
+edit_menu.add_command(label='Paste', command=paste)
+
+help_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label='Help', menu=help_menu)
+help_menu.add_command(label='About', command=about)
+
+
 
 window.mainloop()
